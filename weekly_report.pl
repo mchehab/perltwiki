a5a5d51a77013078feb819bf92b09609161ce44c
Mauro Carvalho Chehab <m.chehab@samsung.com>
2014-02-01 12:59:33 -0200

    weekly_report.pl: Add a patch list too at the development session
    Lists all developed patches too.
    
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- weekly_report.pl
+++ weekly_report.pl
@@ -12,7 +12,6 @@ my $domain = "";
 my $team = "";
 my $dry_run = 0;
 my $debug = 0;
-my $commits_by_date =  cwd() ."/commits_by_date.sh";
 my $author = "";
 my $help;
 my $force_week;
@@ -54,42 +53,76 @@ my %projects = (
 
 sub get_patch_table($$$)
 {
-	my $date1 = shift;
-	my $date2 = shift;
-	my $summary = shift;
+	my @saturday = @{$_[0]};
+	my @sunday = @{$_[1]};
+	my $summary = $_[2];
 
 	my $table = "";
 
-	my $stats_line = "$commits_by_date --since $date1 --to $date2 --name chehab";
-	$stats_line .= "--silent" if ($summary);
+	# This is to avoid digging too deeper at the tree
+	my $date1 = sprintf "%04d-%02d-%02d", 2013, 1, 1;
 
 	foreach my $proj (keys %projects) {
 		my $dir = $projects{$proj};
 		my $per_author;
 		my $per_committer;
 
+		my $since = (Date_to_Days(@saturday) - Date_to_Days(1970, 01, 01)) * 60 * 60 * 24;
+		my $to = (Date_to_Days(@sunday) - Date_to_Days(1970, 01, 01) + 1) * 60 * 60 * 24 - 1;
+
 		if ($summary) {
-			$per_author = qx(cd $dir && $commits_by_date --author --since $date1 --to $date2 --name '$name' --silent);
-			$per_committer = qx(cd $dir && $commits_by_date --committer --since $date1 --to $date2 --name '$name' --silent);
+			open IN, "cd $dir && git log --date=raw --format='%h|%ad|%an|%cd|%cn' --date-order --since '$date1' |grep '$name'|";
+			while (<IN>) {
+				if (m/([^\|]+)\|([^\|\s]+)\s+[^\|]+\|([^\|]+)\|([^\|]+)\s+[^\|]+\|([^\|]+?)\s*$/) {
+					my $cs = $1;
+					my $ad = $2;
+					my $an = $3;
+					my $cd = $4;
+					my $cn = $5;
+
+					$per_author++ if ($ad >= $since && $ad <= $to && $an =~ m/($name)/);
+					$per_committer++ if ($cd >= $since && $cd <= $to && $cn =~ m/($name)/);
+				}
+			}
+
+			my $reviewed = $per_committer;
+
+			$reviewed -= $per_author if ($reviewed >= $per_author);
+
+			next if ($reviewed == 0 && $per_author == 0 && $per_committer == 0);
+
+			printf "\tproject $proj, directory $dir: %d authored, %d committed, %d reviewed\n", $per_author, $per_committer, $reviewed if ($debug);
+
+			$table .= sprintf "---+++ $proj Patch Summary\n%%TABLE{headerrows=\"1\"}%%\n";
+			$table .= sprintf '| *Submitted* | *Committed* | *Reviewed* | *GBM Requested* | *Notes/Collection Mechanism* |';
+			$table .= "\n| " . $per_author . " | " . $per_committer.	" | " .	$reviewed;
+			$table .= " | 0 | [[MauroChehabPerlTwiki][Mauro Chehab report's own mechanism]] |\n\n";
+
+			close IN;
 		} else {
-#			$per_author = qx(cd $dir && $commits_by_date --author --since $date1 --to $date2 --name '$name');
-#			$per_committer = qx(cd $dir && $commits_by_date --committer --since $date1 --to $date2 --name '$name');
+			my $patch = "";
+			open IN, "cd $dir && git log --date=raw --format='%h|%ad|%an|%cd|%cn|%s' --date-order --since '$date1' |grep '$name'|";
+			while (<IN>) {
+				if (m/([^\|]+)\|([^\|\s]+)\s+[^\|]+\|([^\|]+)\|([^\|]+)\s+[^\|]+\|([^\|]+)\|([^\|]+?)\s*$/) {
+					my $cs = $1;
+					my $ad = $2;
+					my $an = $3;
+					my $cd = $4;
+					my $cn = $5;
+					my $s = $6;
+
+					next if (($ad < $since || $ad > $to) && ($cd < $since || $cd > $to));
+
+					$patch .= "| $cs | $an | $cn | $s |\n";
+				}
+			}
+			close IN;
+			if ($patch ne "") {
+				$table .= sprintf "---+++ $proj Patch Summary\n%%TABLE{headerrows=\"1\"}%%\n";
+				$table .= sprintf '| *Changeset* | *Author* | *Comitter* | *Subject* |';
+				$table .= "\n$patch\n";
+			}
 		}
-
-		$per_author =~ s/\s+$//;
-		$per_committer =~ s/\s+$//;
-		my $reviewed = $per_committer;
-
-		$reviewed -= $per_author if ($reviewed >= $per_author);
-
-		next if ($reviewed == 0 && $per_author == 0 && $per_committer == 0);
-
-		printf "\tproject $proj, directory $dir: %d authored, %d committed, %d reviewed\n", $per_author, $per_committer, $reviewed if ($debug);
-
-		$table .= sprintf "---+++ $proj Patch Summary\n%%TABLE{headerrows=\"1\"}%%\n";
-		$table .= sprintf '| *Submitted* | *Committed* | *Reviewed* | *GBM Requested* | *Notes/Collection Mechanism* |';
-		$table .= "\n| " . $per_author . " | " . $per_committer.	" | " .	$reviewed;
-		$table .= " | 0 | [[MauroChehabPerlTwiki][Mauro Chehab report's own mechanism]] |\n\n";
 	}
 	return $table;
 }
@@ -131,9 +164,6 @@ my @saturday = Add_Delta_Days(Monday_of_Week($week, $year), -1);
 my @sunday = Add_Delta_Days(Monday_of_Week($week, $year), 5);
 
 printf "From %04d-%02d-%02d to %04d-%02d-%02d\n", @saturday, @sunday if ($debug);
-
-my $date1 = sprintf "%04d-%02d-%02d", @saturday;
-my $date2 = sprintf "%04d-%02d-%02d", @sunday;
 
 my $month1 = Month_to_Text($saturday[1]);
 
@@ -189,8 +219,8 @@ if ($empty) {
 	$data .= sprintf "\n\n-- Main.$username - %04d-%02d-%02d\n", $year, $month, $day;
 }
 
-$data = replace_table($sum_table_tag, $data, 'Summary', $date1, $date2, 1);
-$data = replace_table($patch_table_tag, $data, 'Development', $date1, $date2, 0);
+$data = replace_table($sum_table_tag, $data, 'Summary', \@saturday, \@sunday, 1);
+$data = replace_table($patch_table_tag, $data, 'Development', \@saturday, \@sunday, 0);
 
 print $data if ($debug);
 exit if ($dry_run);
